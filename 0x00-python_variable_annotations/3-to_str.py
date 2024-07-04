68cd7c29285f0c445a0d2c760de02259442d43c2
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-04 21:58:29 +0100

    To String

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module - 3-to_str"""
+
+
+def to_str(n: float) -> str:
+    """The to_str function returns a string representation
+    of a floating pointnumber
+    """
+    return str(n)
