d64430dac412346621a9210b6459fa452308442d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-04 20:11:20 +0000

    Add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin env python3
-
 """Module - 0-add: Defines a function that adds two numbers together."""
 
 
 def add(a: float, b: float) -> float:
     """The add function adds to float numbers
-
-    Args:
-        a (float)
-        b (float)
     """
     return a + b
