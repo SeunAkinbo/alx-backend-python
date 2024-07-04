8c50d5726bcb850276b4d333be3d6b0e1b165cf1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-04 23:04:36 +0100

    Multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module - 8-make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """The make_multiplier function returns a function
+    """
+    return lambda x: x * multiplier
