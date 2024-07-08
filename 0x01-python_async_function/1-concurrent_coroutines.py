fd200fea96d8e640e365b2c5b5fddfb47f7e1fbf
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-08 18:48:34 +0100

    Multiple Co-routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n function
+    """
+    result = []
+    while n:
+        delay = await wait_random(max_delay)
+        result.append(delay)
+        n -= 1
+    return sorted(result)
