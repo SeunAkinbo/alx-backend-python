22809827f3e3907f73ecce25edd631a0eb1b8f22
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-08 20:28:51 +0100

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """Module - 4-tasks.py"""
+import asyncio
+from typing import List
+import time
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """measure_time function
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
