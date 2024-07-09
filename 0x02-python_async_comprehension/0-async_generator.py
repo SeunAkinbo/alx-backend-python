5c129d72cff1b3f3ec9165206b4fc7d5e02c1b9d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-09 22:36:38 +0100

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """The async_generator function returns a generator
     """
     for _ in range(10):
